c7801151a16457fd1af7e30cdcd368a613588d1c
Tilak Rana <114658444+tilak-rana@users.noreply.github.com>
2025-05-02 14:02:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://getjobportfolio.netlify.app",
   credentials: true,
 };
 
